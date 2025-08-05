faf3d227327be438848eca5baa397ad86414013d
TahirMuradov <muradovtahir01@gmail.com>
2025-08-05 15:06:50 +0400

    BiDirectional Communication

---
+++ Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Net.Client;
+using gRPCServer;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5212");
+var biDirectionalMessage = new BiDirectionalMessage.BiDirectionalMessageClient(channel);
+CancellationToken cancellationToken = new CancellationToken();
+var task1 = Task.Run(async () =>
+{
+    using (var call = biDirectionalMessage.SayHello())
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine($"Sending message to server {i + 1}");
+            await call.RequestStream.WriteAsync(new BiDirectionalMessageRequest
+            {
+                Name = "Tahir",
+                Index = i
+            });
+            if (await call.ResponseStream.MoveNext(cancellationToken))
+            {
+                await Task.Delay(1000);
+
+                Console.WriteLine("from Server " + call.ResponseStream.Current.Message + " " + call.ResponseStream.Current.Index);
+
+
+            }
+        }
+        await call.RequestStream.CompleteAsync();
+
+    }
+
+});
+await task1;
